975200c1387163b7b772c3ded10c449ec4f5826d
unknown <jakarizkifahriza@gmail.com>
2020-10-26 20:58:18 +0700

    Latihan Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,38 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //method tekan tombol
+  int number = 0;
+  void tekantombol() {
+    number = number + 1;
+    setState(() {
+      number = number + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(title: Text("Applikasi Stateless Widget Hello Word")),
-      body: Center(child: Text("Hello Word")),
-    ));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Stateful Widget"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(number.toString()),
+              RaisedButton(child: Text("Tambah"), onPressed: tekantombol)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
